11e8d80d4ee524eba9348b554fc1a7b08f5e1b76
unknown <dsappert@uni.mainz.de>
2017-09-28 09:35:09 +0200

    Zielgerade

--- team04_app01_splashScreenDesign/app/src/main/java/com/example/l_pba/team04_app01_splashscreendesign/DataActivity.java
+++ team04_app01_splashScreenDesign/app/src/main/java/com/example/l_pba/team04_app01_splashscreendesign/DataActivity.java
@@ -112,6 +112,7 @@ public class DataActivity extends AppCompatActivity {
 
     private void GetCaption(){
         String[] empty = new String[]{}; //String if preferences.isEmpty
+        allItems.clear();
 
         if (!preferences.getAll().isEmpty()) {
             prefArray = preferences.getAll().keySet().toArray(new String[0]);
